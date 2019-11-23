a7a76042a3773e9b357a4be2802e5314ff4e3b7c
Almaz <khaliullin@karelshungit.com>
2019-11-23 13:35:36 +0300

    Added db local file

--- src/config/db.php
+++ src/config/db.php
@@ -1,14 +1,19 @@
 <?php
+$file_name = __DIR__ . '/' . '_local.db.php';
+$include_db_config = [];
+if (file_exists($file_name)) {
+    $include_db_config = include $file_name;
+}
 
-return [
+return array_replace_recursive([
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=postgres;port=5432;dbname=touchme',
-    'username' => 'touchme',
-    'password' => 'qwerty',
+    'dsn' => 'pgsql:host=postgres;port=5432;dbname=db_name',
+    'username' => 'user_name',
+    'password' => 'password',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
-];
+], $include_db_config);
